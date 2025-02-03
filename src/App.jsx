afadee25a18dd9ed23d2fcee9a0702287bcf9b00
Gaurav kaushik <connect.gauravkaushik@gmail.com>
2025-02-03 13:38:12 +0530

    updating the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
 import HeaderComp from "./components/HeaderComp.jsx";
-import { MyProvider } from "./utils/Myprovider.jsx";
-
 function App() {
   return (
     <>
-    <MyProvider>
+
     <HeaderComp />
-    </MyProvider>
 
     </>
   );
